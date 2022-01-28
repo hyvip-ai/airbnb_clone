26c2ba319ea50f04b94c4f3909733b1df457a550
hyvip-ai <rm2932002@gmail.com>
2022-01-29 01:07:17 +0530

    Explore Nearby added

---
+++ components/SmallCard.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+
+function SmallCard({ location, image, distace }) {
+  return (
+    <div className="relative cursor-pointer m-3 h-[100px] flex items-center justify-between hover:bg-gray-100 transition duration-200 ease-in-out hover:scale-105 rounded-xl">
+      <div className="relative h-full w-2/5">
+        <Image
+          src={image}
+          alt={location}
+          layout="fill"
+          objectFit="cover"
+          objectPosition="left"
+          className="rounded-xl"
+        />
+        
+      </div>
+      <div className="w-2/5 text-sm">
+        <p>{location}</p>
+        <p className="text-gray-500">{distace}</p>
+      </div>
+    </div>
+  );
+}
+
+export default SmallCard;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import React from 'react'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
+import Head from "next/head";
+import React from "react";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import SmallCard from "../components/SmallCard";
+export default function Home({ exploreData }) {
   return (
     <React.Fragment>
       <Head>
         <title>AirBnB - Clone | NextJs</title>
       </Head>
-        <Header />
-        <Banner />
-
+      <Header />
+      <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+            {exploreData?.map(({ location, img, distance }) => {
+              return (
+                <SmallCard
+                  location={location}
+                  image={img}
+                  distace={distance}
+                  key={img}
+                />
+              );
+            })}
+          </div>
+        </section>
+      </main>
     </React.Fragment>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`https://links.papareact.com/pyp`, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+  const exploreData = await res.json();
+  return {
+    props: { exploreData },
+  };
 }
